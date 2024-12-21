fe51c303263e999e4aac3a2fa7115f761a23db71
Dani <dany12rp@hotmail.com>
2024-12-21 15:44:01 -0300

    Termino con endpoints y migro de sqlserver a sqlite

---
+++ CategoryService/Controllers/CategoryController.cs
@@ -0,0 +1,106 @@
+﻿using CategoryService.DTOs;
+using CategoryService.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace CategoryService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        ICategoryRepository categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetCategories()
+        {
+            try
+            {
+                var categories = await categoryRepository.GetCategories();
+                return Ok(categories);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}", Name = "CategoryById")]
+        public async Task<ActionResult<CategoryDTO>> GetCategory(int id)
+        {
+            try
+            {
+                var category = await categoryRepository.GetCategoryById(id);
+
+                if (category == null) return NotFound();
+
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(AddCategoryDTO category)
+        {
+            try
+            {
+                if (category == null) return BadRequest();
+
+                var categoryCreated = await categoryRepository.CreateCategory(category);
+
+                return CreatedAtRoute("CategoryById", new {id =  categoryCreated.Id}, categoryCreated);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCategory(int id, AddCategoryDTO category)
+        {
+            try
+            {
+                if(category == null) return BadRequest();
+
+                await categoryRepository.UpdateCategory(id, category);
+                return NoContent();
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            try
+            {
+                await categoryRepository.DeleteCategory(id);
+                return NoContent();
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
