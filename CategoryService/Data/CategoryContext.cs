580c76d9ca56669bad05be4f6d7470d69cde4c80
Dani <dany12rp@hotmail.com>
2024-12-25 14:30:55 -0300

    Agrego creacion automatica de base de datos

--- CategoryService/Data/CategoryContext.cs
+++ CategoryService/Data/CategoryContext.cs
@@ -1,11 +1,25 @@
 ﻿using CategoryService.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace CategoryService.Data
 {
     public class CategoryContext : DbContext
     {
-        public CategoryContext(DbContextOptions<CategoryContext> options) : base(options) { }
+        public CategoryContext(DbContextOptions<CategoryContext> options) : base(options) 
+        {
+            var dbCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+            if (dbCreator != null )
+            {
+                if (!dbCreator.CanConnect())
+                    dbCreator.Create();
+
+                if(!dbCreator.HasTables())
+                    dbCreator.CreateTables();
+            }
+
+        }
         public DbSet<Category> Categories { get; set; }
     }
 }
